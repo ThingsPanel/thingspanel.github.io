08566b8d2ed37c347fcf2798a44d2ce06c74db03
he-zhuozhuo <421045249@qq.com>
2022-08-02 14:45:11 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,14 +38,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ThingsPanel/thingspanel.github.io/tree/source/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ThingsPanel/thingspanel.github.io/tree/source/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
