ccef35cf0951a4690519013af5c3b59efa14e584
Dabao-18 <1500905752@qq.com>
2022-09-16 16:40:51 +0800

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -73,11 +73,9 @@ export default function Home(): JSX.Element {
               <span className="footer__link-separator">&middot;</span>
               <a className="footer__link-item" href="#url">Contribute</a> */}
             </div>
-            <div><Translate
-                  id="bottom.ofTheInformation"
-                  description="bottom messgae">
-                Copyright © 2022 北京极益科技有限公司 版权所有. 
-                </Translate></div>
+            <div>
+                <Translate>bottom.ofTheInformation</Translate>
+            </div>
                 {/* Copyright © 2022 北京极益科技有限公司 版权所有. */}
                 {/* Copyright © 2022 Beijing jiyi Technology Co., LTD. All Rights reserved.  */}
 
