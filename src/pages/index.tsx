75e5ce155bfa22992382c30218f96f50b440de4f
hezhuozhuo <50697482+hezhuozhuo@users.noreply.github.com>
2022-10-24 15:18:41 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,13 +37,7 @@ function HomepageHeader() {
           </Link>
           </span>
         </div>
-
       </div>
-      <div>
-    <video id="video" controls="" autoplay="true" loop="loop" preload="none" poster="封面">
-      <source id="mp4" src="http://dev.thingspanel.cn:9999/files/tp.mp4" type="video/mp4">
-    </videos>
-     </div>
     </header>
   );
 }
